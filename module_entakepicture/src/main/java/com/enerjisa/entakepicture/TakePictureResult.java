bc17297c012a81e99c0dce1ca7767d90f174a6f4
Arda Kaplan <ardakaplan101@gmail.com>
2021-08-06 12:45:06 +0300

    gradles scripts

---
+++ module_entakepicture/src/main/java/com/enerjisa/entakepicture/TakePictureResult.java
@@ -0,0 +1,14 @@
+package com.enerjisa.entakepicture;
+
+/**
+ * Created by Arda Kaplan at 13.12.2020 - 00:16
+ * <p>
+ * ardakaplan101@gmail.com
+ */
+public enum TakePictureResult {
+
+    SUCCESS,
+    ERROR,
+    NO_CAMERA,
+    USER_CANCELED
+}
